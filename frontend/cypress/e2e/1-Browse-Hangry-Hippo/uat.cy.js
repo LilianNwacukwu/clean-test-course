786a652dcad59de9fc454451a845b8e89f801f3d
LilianNwachukwu <lilydiamond100@gmail.com>
2025-11-16 21:34:13 +0100

    Add acceptance_test job to GitHub Actions workflow for UAT automation

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,11 +1,21 @@
 describe('Shows Appeteasers', () => {
-  it('Connect to Dev Server', () => {
+  it('Connects to Dev Server', () => {
     cy.visit('https://hangryhippo.quantic.host/');
   });
+
   it('selects Handhelds', () => {
-    //TODO: Add a test to check Handhelds
+    cy.contains('Handhelds').click();
+    cy.contains('Cheese Burger').should('exist');
+    cy.contains('Fajita Tacos').should('exist');
+    cy.contains('Tater Tots').should('not.exist');
+    cy.contains('Buffalo Wings').should('not.exist');
   });
+
   it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
+    cy.contains('Appeteasers').click();
+    cy.contains('Tater Tots').should('exist');
+    cy.contains('Buffalo Wings').should('exist');
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
   });
 });
