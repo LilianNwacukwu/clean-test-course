693bba4b52550b359c80da67466920e472ffd43b
LilianNwachukwu <lilydiamond100@gmail.com>
2025-11-16 23:40:45 +0100

    test: add UATs and CI config for lesson 6

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,21 +1,18 @@
 describe('Shows Appeteasers', () => {
-  it('Connects to Dev Server', () => {
+  it('Connect to Dev Server', () => {
     cy.visit('https://hangryhippo.quantic.host/');
   });
-
   it('selects Handhelds', () => {
     cy.contains('Handhelds').click();
-    cy.contains('Cheese Burger').should('exist');
-    cy.contains('Fajita Tacos').should('exist');
-    cy.contains('Tater Tots').should('not.exist');
-    cy.contains('Buffalo Wings').should('not.exist');
+    cy.contains('Cheese Burger');
+    cy.contains('Fajita Tacos');
+    //TODO: Add a test to check Handhelds
   });
-
   it('selects Appeteasers', () => {
     cy.contains('Appeteasers').click();
-    cy.contains('Tater Tots').should('exist');
-    cy.contains('Buffalo Wings').should('exist');
-    cy.contains('Cheese Burger').should('not.exist');
-    cy.contains('Fajita Tacos').should('not.exist');
+    cy.contains ('Cheese Burger'). should( 'not.exist'); 
+    cy.contains ('Fajita Tacos'). should( 'not.exist');
+
+    //TODO: Add a test to check Appeteasers
   });
 });
